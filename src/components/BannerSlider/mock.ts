be719051c07b16d47754ad1bdcd64839b9e40cd1
Mike Santos <iddeiasimplesmike@gmail.com>
2022-12-08 11:29:18 -0300

    uso do componente de imagem do next no banner e no story de banner slider

--- src/components/BannerSlider/mock.ts
+++ src/components/BannerSlider/mock.ts
@@ -1,14 +1,17 @@
 export default [
   {
     img: 'comedouro-postural',
+    title: 'Comedouro Postural Feito Sob Medida',
     htmlTitle: 'Comedouro<br>postural<br>feito sob<br>medida'
   },
   {
     img: 'monte-seu-comedouro',
+    title: 'Monte seu Comedouro Personalizado',
     htmlTitle: 'Monte seu<br>Comedouro<br>Personalizado'
   },
   {
     img: 'saude-e-conforto',
+    title: 'Saúde e Conforto para o seu aumigo',
     htmlTitle: 'Saúde e<br>Conforto<br>para o seu<br>aumigo'
   }
 ]
